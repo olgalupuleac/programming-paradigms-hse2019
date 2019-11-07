bf6ef2e2c69c98083a9cc5240713415fbf0bd284
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-11-07 09:48:34 +0300

    Remove spaces in bad code

--- 191107/bad_code.py
+++ 191107/bad_code.py
@@ -3,31 +3,34 @@ import sys
 
 
 if __name__ == '__main__':
-    filename = sys.argv[1]
-    words = []
+    filename=sys.argv[1]
+    words=[]
     f = open(filename, "r")
     for line in f:
         words.extend(line.split())
     f.close()
-    d = {}
+    d={}
     for i in words:
         if i in d:
-            d[i] = d[i] + 1
+            d[i]=d[i]+1
         else:
-            d2 = {i: 1}
+            d2={i: 1}
             d.update(d2)
-    l2 = []
-    j = 0
-    while j < 20:
-        s = 0
+    l2=[]
+    j=0
+    res=[]
+    while j<20:
+        s=0
         for i in d:
-            if (i not in l2) and (d[i] > s):
-                s = d[i]
-                k = i
+            if (i not in l2) and (d[i]>s):
+                s=d[i]
+                k=i
         if k not in l2:
             l2.append(k)
-        j = j + 1
-    for i in l2:
+            res.append((k, s))
+        j=j+1
+    print(len(res))
+    for i in res:
         print(i)
 
 
