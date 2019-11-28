e113523334e0d796f357d2472eef27f4a72a3383
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-11-28 21:51:00 +0300

    add stub for count task

--- 191128/src/main.cpp
+++ 191128/src/main.cpp
@@ -3,19 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void* worker(void* arg) {
-    printf("Hello from thread! arg=%d\n", *(int*)arg);
-    *(int*)arg += 10;
-    return arg;
+int data = 1234;
+
+void* worker(void*) {
+    printf("Hello from thread! data=%d\n", data);
+    data += 10;
+    return NULL;
 }
 
 int main() {
     pthread_t id;
-    int data = 1234;
     assert(pthread_create(&id, NULL, worker, &data) == 0);
-    void* retval;
-    assert(pthread_join(id, &retval) == 0);
-    assert(retval == &data);
+    assert(pthread_join(id, NULL) == 0);
     printf("data is %d\n", data);
     return 0;
 }
